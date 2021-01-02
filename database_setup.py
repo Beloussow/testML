f537197f997160aad941edf4798632234e4768f6
Belousov N <nbeloussow1978@gmail.com>
2021-01-02 13:31:14 +0500

    2

--- database_setup.py
+++ database_setup.py
@@ -1,10 +1,10 @@
-import sys  
+import sys
+import pandas as pd  
 
 from sqlalchemy import Column, ForeignKey, Integer, String  
 from sqlalchemy.ext.declarative import declarative_base  
 from sqlalchemy.orm import relationship  
 from sqlalchemy import create_engine  
-
 
 Base = declarative_base()  
   
